48cb3680d1b91b57099b622a6cb885ad7e76b10f
boltrsss <163812338+boltrsss@users.noreply.github.com>
2024-10-14 14:59:07 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,187 @@
+$(document).ready(function () {
+    document.getElementById("step_id_1").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        This is not your typical social network\n' +
+        '                    </h5>\n' +
+        '                    <p class=card-text>\n' +
+        '                        You will simply choose a woman and ask to fuck!\n' +
+        '                        <br/><img class=img-fluid src=./images/image11.png alt="Better Than Tinder"/></p>';
+    document.getElementById("step_id_2").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                            IMPORTANT!\n' +
+        '                        </h5>\n' +
+        '                        <p class=card-text>\n' +
+        '                            Before we can show you a list and photos of women who live near you and are ready to\n' +
+        '                            <strong class="purple underline">\n' +
+        '                                have sex right now\n' +
+        '                            </strong>\n' +
+        '                            , we need to ask 3 quick questions to make sure you qualify.\n' +
+        '                        </p><p></p>';
+    document.getElementById("step_id_3").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        QUESTION #1\n' +
+        '                    </h5>\n' +
+        '                    <p class=card-text>\n' +
+        '                        Many of these women are desperate single moms and cheating wives looking for some fun. They\n' +
+        '                        could be your neighbors or someone you know. Do you agree to keep the identity of these women a\n' +
+        '                        secret?\n' +
+        '                    </p><p></p>';
+    document.getElementById("step_id_4").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        QUESTION #2\n' +
+        '                    </h5>\n' +
+        '                    <p class=card-text>\n' +
+        '                        These women have asked us to not allow men that are seeking a "relationship". They only desire\n' +
+        '                        quick sex. Not dating. Do you agree to this request?\n' +
+        '                    </p><p></p>';
+    document.getElementById("step_id_5").innerHTML = ' <h5 class="card-title rounded p-1">\n' +
+        '                        QUESTION #3\n' +
+        '                    </h5>\n' +
+        '                    <p class=card-text>\n' +
+        '                        Are you at least 24 years old? The women have requested that we do not let those younger than 24\n' +
+        '                        contact them because of past rude behavior by younger men.\n' +
+        '                    </p><p></p>';
+    document.getElementById("step_id_6").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        What type of body do you prefer?\n' +
+        '                    </h5>\n' +
+        '                    <div class=checkbox_wrap>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/body1_o.jpg alt=Skinny>\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Skinny\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/body2_o.jpg alt=Regular>\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Regular\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/body3_o.jpg alt=BBW>\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                BBW\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/body4_o.jpg\n' +
+        '                                                                        alt="Big tits are a must">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Big tits are a must\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/body5_o.jpg\n' +
+        '                                                                        alt="Sexy ass is a must">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Sexy ass is a must\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                    </div>';
+    document.getElementById("step_id_7").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                            How old do you like your women?\n' +
+        '                        </h5>\n' +
+        '                        <div class=checkbox_wrap>\n' +
+        '                            <div class="option photo_option tr-option"><img src=./images/age1_o.jpg alt="18 - 25">\n' +
+        '                                <p class="option-title clearfix">\n' +
+        '                                    18 - 25\n' +
+        '                                </p>\n' +
+        '                            </div>\n' +
+        '                            <div class="option photo_option tr-option"><img src=./images/age2_o.jpg alt="26 - 35">\n' +
+        '                                <p class="option-title clearfix">\n' +
+        '                                    26 - 35\n' +
+        '                                </p>\n' +
+        '                            </div>\n' +
+        '                            <div class="option photo_option tr-option"><img src=./images/age3_o.jpg alt="36 - 45">\n' +
+        '                                <p class="option-title clearfix">\n' +
+        '                                    36 - 45\n' +
+        '                                </p>\n' +
+        '                            </div>\n' +
+        '                            <div class="option photo_option tr-option"><img src=./images/age4_o.jpg alt="46 - 55">\n' +
+        '                                <p class="option-title clearfix">\n' +
+        '                                    46 - 55\n' +
+        '                                </p>\n' +
+        '                            </div>\n' +
+        '                            <div class="option photo_option tr-option"><img src=./images/age5_o.jpg alt=55+>\n' +
+        '                                <p class="option-title clearfix">\n' +
+        '                                    55+\n' +
+        '                                </p>\n' +
+        '                            </div>\n' +
+        '                        </div>';
+    document.getElementById("step_id_8").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        What type of relationship are you looking for?\n' +
+        '                    </h5>\n' +
+        '                    <div class=checkbox_wrap>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/relations1_o.jpg\n' +
+        '                                                                        alt="One night stand">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                One night stand\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/relations2_o.jpg\n' +
+        '                                                                        alt="Sex on multiple occasions">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Sex on multiple occasions\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/relations3_o.jpg alt="Regular sex">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Regular sex\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/relations4_o.jpg\n' +
+        '                                                                        alt="Serious Dating">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Serious Dating\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/relations5_o.jpg alt=Marriage>\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Marriage\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                    </div>';
+    document.getElementById("step_id_9").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        Distance between you and her?\n' +
+        '                    </h5>\n' +
+        '                    <div class=checkbox_wrap>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/dist1_o.jpg\n' +
+        '                                                                        alt="Within walking distance">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Within walking distance\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/dist2_o.jpg\n' +
+        '                                                                        alt="Sex on multiple occasions">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Same city\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/dist3_o.jpg alt="Regular sex">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Nearby cities are OK\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/dist4_o.jpg alt="Serious Dating">\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Same country\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                        <div class="option photo_option tr-option"><img src=./images/dist5_o.jpg alt=Marriage>\n' +
+        '                            <p class="option-title clearfix">\n' +
+        '                                Doesn\'t matter\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                    </div>';
+    document.getElementById("step_id_10").innerHTML = '<h5 class="card-title rounded p-1">\n' +
+        '                        Thank you.\n' +
+        '                    </h5>\n' +
+        '                    <p class=card-text>\n' +
+        '                        You may now see our list and photos of women who are in your area and meet your preferences.\n' +
+        '                        Again, please keep their identity a secret\n' +
+        '                    </p>\n' +
+        '                    <p class=card-text>\n' +
+        '                        Click on the "Continue" button below, complete your profile and start looking for women near\n' +
+        '                        you.\n' +
+        '                    </p><p></p>';
+        
+    $(".btn").on("click", function () {
+        $(this).closest(".step").hide().next(".step").fadeIn();
+        $("html, body").animate({scrollTop: 200}, 1000);
+    });
+    
+    $(".option").on("click", function () {
+        $(this).toggleClass("checked tr-selected");
+    });
+});
